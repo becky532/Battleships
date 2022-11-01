e9c04edf69fc5cc8ebd6d439e4ba38499cd494f8
Kanay <kanaysaparia@gmail.com>
2022-11-01 13:59:52 +0000

    ui set up with grid size 7x7

---
+++ battleships/frontEnd.py
@@ -0,0 +1,14 @@
+from flask import render_template, Blueprint, Flask
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+
+    return render_template('battleship.html')
+
+
+if __name__ == "__main__":
+    app.run()
