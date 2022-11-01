7f92ba26372e911abffeddd25c6ebb71fa5a11d9
Becky <becky.sayer@capgemini.com>
2022-11-01 21:03:33 +0000

    Have added a print method to see grid in console

--- battleships/gameBack.py
+++ battleships/gameBack.py
@@ -28,15 +28,17 @@ class Game:
 
         for row in range(self.boardSize):
             self.board.append([0] * self.boardSize)
+
     def printBoard(self):
-        board = self.board
-        board.reverse()
-        for row in board:
-            for column in row:
+
+        listRows = [6, 5, 4, 3, 2, 1, 0]
+
+        for row in listRows:
+            for column in self.board[row]:
                 print(column, end='')
             print()
 
-    def validatePlacement(self, length, row, col):
+    def validateInitialPlacement(self, length, row, col):
 
         placementValid = True
         if col + length <= self.lastIndex:
@@ -51,9 +53,11 @@ class Game:
         return placementValid
 
     def placeShip(self, row, col, shipType):
+        #could be less than 0
         if row <= self.lastIndex and col <= self.lastIndex:
             shipLength = self.shipsDict[shipType]['length']
-            placementValid = self.validatePlacement(shipLength, row, col)
+            placementValid = self.validateInitialPlacement(shipLength, row, col)
+
             if placementValid == True:
                 logging.info(f'Valid placement of {shipType}')
                 shipLength = self.shipsDict[shipType]["length"]
@@ -123,7 +127,7 @@ class Game:
     def __rotate(self, shipType, newPosition):
         self.remove(shipType)
         self.shipsDict[shipType]['position'] = newPosition
-        self.placeShip(shipType)
+        self.place(shipType)
         self.shipsDict[shipType]['orientation'] = 'vertical'
         logging.info(f"Piece {shipType} has been rotated")
 
@@ -137,14 +141,11 @@ if __name__ == '__main__':
     game.placeShip(1, 0, 'Destroyer')
     game.placeShip(4, 2, 'Cruiser')
     game.printBoard()
-    print()
-    game.printBoard()
 
-    # print(game.shipsDict['Submarine'])
-    # game.rotateShip('Submarine')
-    # print(game.shipsDict['Submarine'])
-    # print(game.board)
-    # game.placeShip(6, 1, 'Cruiser')
-    # print(game.board)
-    # game.rotateShip('Cruiser')
-    # game.printBoard()
+    print(game.shipsDict['Submarine'])
+    game.rotateShip('Submarine')
+    print(game.shipsDict['Submarine'])
+    print(game.board)
+    print(game.board)
+    game.rotateShip('Cruiser')
+    game.printBoard()
