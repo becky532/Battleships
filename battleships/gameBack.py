abceafee70e2ba5caf5e4152ddbc45ed762e1a5f
Becky <becky.sayer@capgemini.com>
2022-11-02 09:51:36 +0000

    Added randomise board

--- battleships/gameBack.py
+++ battleships/gameBack.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 
 # class Player:
@@ -9,7 +10,7 @@ import logging
 #     def opponent(self):
 #         if self == Player.WHITE: return Player.BLACK
 #         else: return Player.WHITE
-class Game:
+class Board:
 
     def __init__(self):
         self.board = []
@@ -21,22 +22,36 @@ class Game:
                           }
         self.player = 1
         logging.basicConfig(filename='battleships.log', level=logging.INFO, filemode='w', force=True)
-
-    def createBoard(self):
         self.boardSize = 7
         self.lastIndex = 6
 
-        for row in range(self.boardSize):
-            self.board.append([0] * self.boardSize)
+    def newBoard(self):
+
+        # for row in range(self.boardSize):
+        #     self.board.append([0] * self.boardSize)
+        self.board = [[0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0]]
+
 
     def printBoard(self):
 
         listRows = [6, 5, 4, 3, 2, 1, 0]
 
         for row in listRows:
+            print(str(row) + '--', end='')
             for column in self.board[row]:
-                print(column, end='')
+                print(str(column) + ' ', end='')
             print()
+        print('   ' + '|', '|', '|', '|', '|', '|', '|')
+        print('   ', end='')
+
+        for i in range(7):
+            print(str(i) + ' ', end='')
 
     def validateInitialPlacement(self, length, row, col):
 
@@ -54,6 +69,7 @@ class Game:
 
     def placeShip(self, row, col, shipType):
         #could be less than 0
+
         if row <= self.lastIndex and col <= self.lastIndex:
             shipLength = self.shipsDict[shipType]['length']
             placementValid = self.validateInitialPlacement(shipLength, row, col)
@@ -69,24 +85,37 @@ class Game:
                 self.shipsDict[shipType]['orientation'] = 'horizontal'
             else:
                 logging.info(f'Invalid placement of {shipType}')
+
         else:
             logging.info("Invalid square chosen")
+            placementValid = False
+
+        return placementValid
 
     def remove(self, shipType):
         position = self.shipsDict[shipType]['position']
         for cell in position:
             self.board[cell[0]][cell[1]] = 0
+        self.shipsDict[shipType]['position'] = []
 
     def place(self, shipType):
         position = self.shipsDict[shipType]['position']
         for cell in position:
             self.board[cell[0]][cell[1]] = 1
 
+    def clearBoard(self):
+        self.newBoard()
+        for ship in self.shipsDict:
+            self.shipsDict[ship]['position'] = []
+            self.shipsDict[ship]['orientation'] = 'horizontal'
+
+
     def rotateShip(self, shipType):
         shipPosition = self.shipsDict[shipType]['position']
         rotatingPoint = shipPosition[0]
         shipLength = self.shipsDict[shipType]['length']
         shipOrientation = self.shipsDict[shipType]['orientation']
+        rotationValid = True
 
         shipCol = shipPosition[0][1]
         shipRow = shipPosition[0][0]
@@ -103,6 +132,9 @@ class Game:
             self.shipsDict[shipType]['orientation'] = 'horizontal'
         else:
             logging.info(f"Ship {shipType} could not be rotated")
+            rotationValid = False
+
+        return rotationValid
 
     def __checkRotation(self, length, row, col, orientation, point):
 
@@ -110,7 +142,7 @@ class Game:
         for index in range(1, length):
             if orientation == 'horizontal':
                 newCell = [row + index, col]
-            else:
+            elif orientation == 'vertical':
                 newCell = [row, col + index]
 
             if newCell[0] <= self.lastIndex and newCell[1] <= self.lastIndex:
@@ -131,21 +163,32 @@ class Game:
         self.shipsDict[shipType]['orientation'] = 'vertical'
         logging.info(f"Piece {shipType} has been rotated")
 
+    def randomiseBoard(self):
+        listShip = list(self.shipsDict.keys())
+        random.shuffle(listShip)
+        for ship in listShip:
+            shipPlace = False
+            while shipPlace == False:
+                row = random.randint(0, 6)
+                col = random.randint(0, 6)
+                shipPlace = self.placeShip(row, col, ship)
+            rotate = random.randint(0, 1)
+            if rotate == 1:
+                self.rotateShip(ship)
 
-# create a randomized placement of ships
+
+        return listShip
 
 if __name__ == '__main__':
-    game = Game()
-    game.createBoard()
-    game.placeShip(0, 0, 'Submarine')
-    game.placeShip(1, 0, 'Destroyer')
-    game.placeShip(4, 2, 'Cruiser')
-    game.printBoard()
 
-    print(game.shipsDict['Submarine'])
-    game.rotateShip('Submarine')
-    print(game.shipsDict['Submarine'])
-    print(game.board)
-    print(game.board)
-    game.rotateShip('Cruiser')
-    game.printBoard()
+    board = Board()
+    board.newBoard()
+    # board.placeShip(0, 0, 'Carrier')
+    # board.placeShip(1, 0, 'Battleship')
+    # board.printBoard()
+    # board.clearBoard()
+    # board.printBoard()
+    board.randomiseBoard()
+    board.printBoard()
+    print(board.shipsDict)
+
