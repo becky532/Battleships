d8f67389a427cc1cf5f8d50e11c2b5684a55128d
nozana <noor.zana@capgemini.com>
2022-11-03 09:05:13 +0000

    started console

--- battleships/gameConsole.py
+++ battleships/gameConsole.py
@@ -1,18 +1,124 @@
 from gameBack import Board
+import os
 
 firstPlayerBoard = Board()
 secondPlayerBoard = Board()
 
-firstPlayerBoard.newBoard()
-whichPiece = input('What piece would you like to put down?')
+def menu():
+    print("Hello!")
+    print("Lets go on an adventure!")
+    option = input('''
+                                 ______________________________________________
+                              .-'                     _                        '.
+                            .'                       |-'                        |
+                          .'                         |                          |
+                       _.'               p         _\_/_         p              |
+                    _.'                  |       .'  |  '.       |              |
+               __..'                     |      /    |    \      |              |
+         ___..'                         .T\    ======+======    /T.             |
+      ;;;\::::                        .' | \  /      |      \  / | '.           |
+      ;;;|::::                      .'   |  \/       |       \/  |   '.         |
+      ;;;/::::                    .'     |   \       |        \  |     '.       |
+            ''.__               .'       |    \      |         \ |       '.     |
+                 ''._          <_________|_____>_____|__________>|_________>    |
+                     '._     (___________|___________|___________|___________)  |
+                        '.    \;;;;;;;;;;o;;;;;o;;;;;o;;;;;o;;;;;o;;;;;o;;;;/   |
+                          '.~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~   |
+                            '. ~ ~ ~ ~ ~ ~ ~ ~ ~Battleship ~ ~ ~ ~ ~ ~ ~ ~ ~ ~  |
+                              '-.______________________________________________.'
+    
+    
+    What mode you want to play? :
+    1) Player VS Player
+    2) Player VS Random
+    ''')
 
-if whichPiece == 'Random':
-    firstPlayerBoard.randomiseBoard()
+    if option == '1':
+        firstPlayerSetup()
+        secondPlayerSetup()
+    else:
+        return
 
-# secondPlayerBoard.newBoard()
-# secondPlayerBoard.placeShip(2, 1, 'Destroyer')
-#
-firstPlayerBoard.printBoard()
-# print()
-# secondPlayerBoard.printBoard()
+def firstPlayerSetup():
 
+    print("Player1 Setup")
+
+    firstPlayerBoard.newBoard()
+    listFirstPlayerPieces = list(firstPlayerBoard.shipsDict.keys())
+    firstPlayerBoard.printBoard()
+
+
+    while len(listFirstPlayerPieces) > 0:
+
+        validInput = False
+
+        print("")
+        whichRow = input("Choose a row for your ship: ")
+        whichCol = input("Choose a column for your ship: ")
+        print(listFirstPlayerPieces)
+        while validInput == False:
+            whichShip = input("Choose the ship type: ")
+
+
+            row = int(whichRow)
+            col = int(whichCol)
+            shipType = str(whichShip)
+
+            if shipType not in listFirstPlayerPieces:
+                print("try again")
+            else:
+                validInput = True
+
+
+
+        validPosition = firstPlayerBoard.placeShip(row, col, shipType)
+        if validPosition == False:
+            print("Cannot place ship here")
+            continue
+        else:
+            firstPlayerBoard.printBoard()
+            listFirstPlayerPieces.remove(whichShip)
+
+
+def secondPlayerSetup():
+
+    print("Player1 Setup")
+
+    secondPlayerBoard.newBoard()
+    listSecondPlayerPieces = list(secondPlayerBoard.shipsDict.keys())
+    secondPlayerBoard.printBoard()
+
+
+    while len(listSecondPlayerPieces) > 0:
+
+        validInput = False
+
+        print("")
+        whichRow = input("Choose a row for your ship: ")
+        whichCol = input("Choose a column for your ship: ")
+        print(listSecondPlayerPieces)
+        while validInput == False:
+            whichShip = input("Choose the ship type: ")
+
+
+            row = int(whichRow)
+            col = int(whichCol)
+            shipType = str(whichShip)
+
+            if shipType not in listSecondPlayerPieces:
+                print("try again")
+            else:
+                validInput = True
+
+
+
+        validPosition = firstPlayerBoard.placeShip(row, col, shipType)
+        if validPosition == False:
+            print("Cannot place ship here")
+            continue
+        else:
+            secondPlayerBoard.printBoard()
+            listSecondPlayerPieces.remove(whichShip)
+
+
+menu()
